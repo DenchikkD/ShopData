601128e5add1d9e7e6cc689f7f0beaf1d2c640bc
ilire <Iliren1989@gmail.com>
2017-05-25 22:28:19 +0300

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by ilire on 5/25/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
